eedb8cce5f4790ea5ce627395f29e5bd2ad56aff
Rahul <rahulsuthar90243@gmail.com>
2026-09-02 16:32:47 +0530

    add navbar feature : logo, menu, reach out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import OverlayMenu from "./OverlayMenu";
+import React, { useState, useRef } from "react";
+import Logo3 from "../assets/Logo3.png";
+import { FiMenu } from "react-icons/fi";
+import { useEffect } from "react";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [visible, setVisible] = useState(true);
+  const {forceVisible, setForceVisible} = useState(false)
+
+  const lastScrollY = useRef(0);
+  const timerId = useRef(false);
+
+  useEffect(()=>{
+    const homeSection = document.querySelector("#home");
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if(entry.isIntersecting){
+          setForceVisible(true)
+          setVisible(true)
+        }else{
+          setForceVisible(false);
+        }
+      }, {threshold: 0.1}
+    )
+    if(homeSection) observer.observe(homeSection);
+    return () => {
+      if(homeSection) observer.unobserve(homeSection);
+    }
+  }, [])
+
+  useEffect(() =>{
+     const handleScroll = () => {
+      if(forceVisible){
+        setVisible(true)
+        return 
+      }
+      const currentScrollY = window.scrollY;
+      if(currentScrollY > lastScrollY.current){
+        setVisible(false)
+      }else{
+        setVisible(true)
+        if(timerId.current) clearTimeout(timerId.current);
+        timerId.current = setTimeout(() => {
+          setVisible(false);
+        }, 3000)
+      }
+      lastScrollY.current = currentScrollY;
+     }
+     window.addEventListener("scroll", handleScroll, {passive: true})
+
+     return () => {
+      window.removeEventListener("scroll", handleScroll)
+      if(timerId.current) clearTimeout(timerId.current);
+     }
+  }, forceVisible)
+
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <nav
+        className={`fixed top-0 left-0 w-full relative flex items-center justify-center px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "-translate-y-full"}`}
+      >
+        <div className="absolute left-6 flex items-center">
+          <img src={Logo3} alt="logo" className="mt-8 w-12 h-12" />
+          <div className="mt-12 text-3xl font-bold text-white hidden sm:block">
+            Rahul
+          </div>
+        </div>
+
+        <div className="block lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2">
+          <button
+            onClick={() => setMenuOpen(true)}
+            className="text-white text-3xl focus:outline-none mt-4 cursor-pointer"
+          >
+            <FiMenu />
+          </button>
+        </div>
+
+        <div className="hidden lg:block absolute right-6 top-1/2 -translate-y-1/2 mt-4">
+          <a
+            href="#contect"
+            className="bg-gradient-to-r from-pink-500 to-blue-500 text-white px-5 py-2 rounded-full font-medium shadow-lg hover:opacity-90 transition-opacity duration-300"
+          >
+            Reach Out
+          </a>
+        </div>
+
+      </nav>
+
+      <OverlayMenu isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
