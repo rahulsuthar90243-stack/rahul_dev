38d19722b6636e7707efe53e3cd492167786301c
Rahul <rahulsuthar90243@gmail.com>
2026-09-08 15:57:45 +0530

    debug code

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -83,7 +83,7 @@ function ParticlesBackground() {
 
   return (
     
-    <canvas ref={canvasRef} className='fixed top-0 left-0 w-full h-full pointer-events-none z-0'>
+    <canvas ref={canvasRef} className='absolute top-0 left-0 w-full h-full pointer-events-none z-0'>
 
     </canvas>
   )
--- src/section/Home.jsx
+++ src/section/Home.jsx
@@ -118,7 +118,7 @@ function Home() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
           >
-            Hello I'm <br />
+            Hello, I'm <br />
             <span className="text-white font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl lg:whitespace-nowrap ">
               Rahul Suthar
             </span>
