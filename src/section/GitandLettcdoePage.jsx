3834abe2eaf330e9c62fe79858d792417150cef0
Rahul <rahulsuthar90243@gmail.com>
2026-09-10 16:29:15 +0530

    Add Git and LeetCode page

---
+++ src/section/GitandLettcdoePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LeetCode from "../components/LeetCode"
+
+function GitandLettcdoePage() {
+  return (
+    <div className='w-full min-h-screen flex items-center justify-center px-4 py-10'>
+      <LeetCode />
+    </div>
+  )
+}
+
+export default GitandLettcdoePage
