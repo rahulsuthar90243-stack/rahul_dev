2953e0ff5d50182981a7c4d49f58fa017486048f
Rahul <rahulsuthar90243@gmail.com>
2026-09-12 16:31:56 +0530

    add githubRouter api path

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import "dotenv/config";
-import cors from "cors";    
+import cors from "cors";      // this middleware is used to share resources between different origins (domains) in web applications
 import express from "express";
 import leetcodeRouter from "./routers/leetcode.router.js"
+import githubRouter from "./routers/github.router.js"
 
 
 const app = express();
@@ -17,6 +18,7 @@ app.get("/health", (_request, response) => {
 
 
 app.use("/api/leetcode", leetcodeRouter);  // Mount the leetcodeRouter at the /api/leetcode path
+app.use("/api/github", githubRouter)  
 
 app.listen(port, () => {
   console.log(`LeetCode backend running at http://localhost:${port}`);
