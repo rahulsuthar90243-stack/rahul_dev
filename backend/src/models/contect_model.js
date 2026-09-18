7fe88d4ee654e653fcd150033e64a754d8b77aa6
Rahul <rahulsuthar90243@gmail.com>
2026-09-18 16:21:29 +0530

    create a contect model

---
+++ backend/src/models/contect_model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const contectSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    service: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    budget: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const contectModel = mongoose.model("contectModel", contectSchema);
